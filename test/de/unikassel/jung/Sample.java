0288be42d327155ebe459827b18cd202cf8ff8f3
Antoschka <timaschew@gmail.com>
2011-05-02 17:46:22 +0200

    jung libraries

--- test/de/unikassel/jung/Sample.java
+++ test/de/unikassel/jung/Sample.java
@@ -8,10 +8,9 @@ import edu.uci.ics.jung.samples.UnicodeLabelDemo;
 import edu.uci.ics.jung.samples.PluggableRendererDemo; // very nice
 
 public class Sample {
-	
 	public static void main (String args[]) throws IOException {
 		PluggableRendererDemo.main(null);
+		UnicodeLabelDemo.main(null);
+		GraphEditorDemo.main(null);
 	}
-	
-
 }
