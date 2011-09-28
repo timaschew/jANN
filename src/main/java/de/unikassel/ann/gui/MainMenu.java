389073938b2e7ac9b329369885ee2fb6475eea1f
Sofia Daskalou <sofia@jann.de>
2011-09-28 18:30:21 +0200

    remove unused innerclass

--- src/main/java/de/unikassel/ann/gui/MainMenu.java
+++ src/main/java/de/unikassel/ann/gui/MainMenu.java
@@ -1,25 +1,16 @@
 package de.unikassel.ann.gui;
 
-import java.awt.Component;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.io.File;
-import java.net.URI;
-import java.net.URISyntaxException;
 
-import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
 import javax.swing.KeyStroke;
 
 import de.unikassel.ann.controller.ActionJMenuItem;
 import de.unikassel.ann.controller.Actions;
-import de.unikassel.ann.controller.GraphController;
 import de.unikassel.ann.controller.Settings;
-import de.unikassel.ann.model.UserSession;
 
 public class MainMenu extends JMenuBar {
 
@@ -171,166 +162,5 @@ public class MainMenu extends JMenuBar {
 		mnHilfe.add(mntmUeber);
 
 		return mnHilfe;
-	}
-
-	/**
-	 * Menu action item class
-	 */
-	private class ActionMenuItem extends JMenuItem implements ActionListener {
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 1L;
-		Actions action;
-
-		public ActionMenuItem(final String text, final Actions action) {
-			super(text);
-			this.action = action;
-			addActionListener(this);
-		}
-
-		@Override
-		public void actionPerformed(final ActionEvent e) {
-			System.out.println(action);
-			switch (action) {
-			case NEW:
-				// TODO
-				// Check if the current session is unsaved -> save?
-				// Create a new Session
-
-				// Clear Graph
-				GraphController.getInstance().reset();
-
-				// Reset Sidebar
-				Settings.getInstance().createNewSession(Settings.getI18n("session.initial.name"));
-
-				break;
-			case IMPORT:
-				ImportFilePanel panel = new ImportFilePanel();
-				panel.setVisible(true);
-				break;
-			case EXPORT:
-				ExportSaveFilePanel export = new ExportSaveFilePanel();
-				export.setVisible(true);
-				break;
-			case CLOSE_CURRENT_SESSION:
-				Settings.getInstance().getCurrentSession();
-				// TODO soll die current session beim schließen gespeichert werden?
-
-				break;
-			case CHANGE_BETWEEN_SESSIONS:
-				// TODO aktuelle Session auf die neue ausgewählte setzen
-				System.out.println(e.getActionCommand());
-				UserSession current = Settings.getInstance().getCurrentSession();
-				System.out.println("Current: " + current);
-				// loadNetworkFromFile(e.getActionCommand());
-				// NetConfig netConfig = new NetConfig();
-				// Settings.getInstance().getCurrentSession().setNetworkConfig(netConfig);
-				// Main.instance.getGraphLayoutViewer().renderNetwork(netConfig.getNetwork());
-				break;
-			case VIEW_DATA:
-				// TODO
-				break;
-			case VIEW_TRAINING:
-				// TODO
-				break;
-			case ABOUT:
-				JFrame dialog = new JFrame();
-				JOptionPane.showMessageDialog(dialog, "Anton Wilhelm, \n" + "Alexander Vey, \n" + "Sofia Daskalou, 26201618 \n",
-						"Teilnehmer", JOptionPane.INFORMATION_MESSAGE);
-				// TODO
-				break;
-			case EXIT:
-
-				Object[] options = { "Speichern", "Nein", "Abbrechen" };
-				Component frame = new JFrame();
-				int n = JOptionPane.showOptionDialog(frame, "Die aktuelle Session ist nicht gespeichert \n" + "Möchten Sie speichern?",
-						"Speichern", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[2]);
-
-				if (n == JOptionPane.YES_OPTION) {
-					// TODO call Export panel
-				} else if (n == JOptionPane.NO_OPTION) {
-					System.exit(e.getID());
-				} else if (n == JOptionPane.CANCEL_OPTION) {
-				}
-
-				break;
-			case LOAD_OR_NETWORK:
-				loadNetworkFromFile("or");
-				break;
-			case LOAD_XOR_NETWORK:
-				loadNetworkFromFile("xor");
-				break;
-			case LOAD_AND_NETWORK:
-				loadNetworkFromFile("and");
-				break;
-			case LOAD_2_BIT_ADDIERER_NETWORK:
-				loadNetworkFromFile("2-bit-addierer");
-				break;
-			case BACKPROPAGATION_VIEW:
-				Main.instance.addBackproSidebarPanel();
-				break;
-			case SOM_VIEW:
-				Main.instance.addSOMSidebarPanel();
-				break;
-			case NORMALIZE_TRAIN_DATA:
-				TrainDataPanel trainData = TrainDataPanel.getTrainDataPanelInstance();
-				trainData.setVisible(true);
-				break;
-			case NONE:
-			default:
-				System.out.println("Unknown command: " + action);
-				break;
-			}
-		}
-
-	}
-
-	/**
-	 * @param string
-	 */
-	private void loadNetworkFromFile(final String name) {
-		Settings settings = Settings.getInstance();
-		File importFile;
-		if (name.equals("or")) {
-			try {
-				importFile = new File(new URI(MainMenu.class.getClassLoader().getResource("net_cfg_or.csv").toString()));
-				settings.loadNetworkFromFile(importFile);
-			} catch (URISyntaxException e) {
-				e.printStackTrace();
-			}
-
-		} else if (name.equals("xor")) {
-			try {
-				importFile = new File(new URI(MainMenu.class.getClassLoader().getResource("net_cfg.csv").toString()));
-				settings.loadNetworkFromFile(importFile);
-			} catch (URISyntaxException e) {
-				e.printStackTrace();
-			}
-
-		} else if (name.equals("and")) {
-			try {
-				importFile = new File(new URI(MainMenu.class.getClassLoader().getResource("net_cfg_and.csv").toString()));
-				settings.loadNetworkFromFile(importFile);
-			} catch (URISyntaxException e) {
-				e.printStackTrace();
-			}
-
-		} else if (name.equals("2-bit-addierer")) {
-			try {
-				importFile = new File(new URI(MainMenu.class.getClassLoader().getResource("net_cfg_2BitAddierer.csv").toString()));
-				settings.loadNetworkFromFile(importFile);
-			} catch (URISyntaxException e) {
-				e.printStackTrace();
-			}
-
-		}
-		addSessionToSubMenu();
-	}
-
-	public void addSessionToSubMenu() {
-		String name = Settings.getInstance().getCurrentSession().toString();
-		JMenuItem mtSession = new ActionMenuItem(name, Actions.CHANGE_BETWEEN_SESSIONS);
-		subMenuSession.add(mtSession);
 	}
 }
