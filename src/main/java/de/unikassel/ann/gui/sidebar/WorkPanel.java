64c17e99be3eff101101d4c137318bf9d2361eec
Alexander Vey <this.vey@gmail.com>
2011-09-30 23:02:29 +0200

    work panel works

--- src/main/java/de/unikassel/ann/gui/sidebar/WorkPanel.java
+++ src/main/java/de/unikassel/ann/gui/sidebar/WorkPanel.java
@@ -9,6 +9,8 @@ package de.unikassel.ann.gui.sidebar;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
@@ -21,7 +23,11 @@ import javax.swing.border.TitledBorder;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
+import de.unikassel.ann.config.NetConfig;
+import de.unikassel.ann.controller.GraphController;
 import de.unikassel.ann.controller.Settings;
+import de.unikassel.ann.model.DataPair;
+import de.unikassel.ann.model.DataPairSet;
 import de.unikassel.ann.util.Logger;
 
 /**
@@ -30,8 +36,17 @@ import de.unikassel.ann.util.Logger;
  */
 public class WorkPanel extends JPanel implements PropertyChangeListener, ChangeListener {
 
-	public static WorkPanel instance = new WorkPanel();
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6425592984651769443L;
+
+	private static final String dataSeparator = ";";
+
 	private Integer inputs;
+	private JButton applyData;
+
+	private JTextField testDataField;
 
 	public WorkPanel() {
 
@@ -57,28 +72,71 @@ public class WorkPanel extends JPanel implements PropertyChangeListener, ChangeL
 
 		removeAll();
 
-		for (int i = 1; i <= inputs; i++) {
-
-			JLabel label = new JLabel(Settings.i18n.getString("workpanel.lblInput") + i);
-			inputsP.add(label);
-			JTextField field = new JTextField(10);
-			fieldsP.add(field);
-
-			add(inputsP);
-			add(fieldsP);
-			inputsP.revalidate();
-			fieldsP.revalidate();
-			validate();
-			repaint();
-
-		}
+		JLabel label = new JLabel(Settings.i18n.getString("workpanel.lblInput"));
+		inputsP.add(label);
+		testDataField = new JTextField(10);
+		fieldsP.add(testDataField);
+		add(inputsP);
+		add(fieldsP);
+		inputsP.revalidate();
+		fieldsP.revalidate();
+		validate();
+		repaint();
 
 		JCheckBox normalizeData = new JCheckBox(Settings.i18n.getString("workpanel.normalizeCB"));
 		inputsP.add(normalizeData);
 
-		JButton applyData = new JButton(Settings.i18n.getString("workpanel.btnApplyData"));
+		applyData = new JButton(Settings.i18n.getString("workpanel.btnApplyData"));
 		fieldsP.add(applyData);
 
+		initListeners();
+	}
+
+	/**
+	 * 
+	 */
+	private void initListeners() {
+		applyData.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(final ActionEvent e) {
+				NetConfig net = Settings.getInstance().getCurrentSession().getNetworkConfig();
+
+				// Get input data
+				String text = testDataField.getText();
+				if (text == null || text.isEmpty()) {
+					// no input available!
+					return;
+				}
+
+				// Split input by the data separator
+				String[] data = text.split(dataSeparator);
+				Double[] input = new Double[data.length];
+				for (int i = 0; i < data.length; i++) {
+					input[i] = Double.valueOf(data[i]);
+				}
+
+				// Create test data
+				DataPairSet testData = new DataPairSet();
+				testData.addPair(new DataPair(input, new Double[] { Double.NaN }));
+
+				// Validate test data
+				int trainInLenght = testData.getPairs().get(0).getInput().length;
+				int trainOutLenght = testData.getPairs().get(0).getIdeal().length;
+				int netInLenght = net.getNetwork().getInputSizeIgnoringBias();
+				int netOutLength = net.getNetwork().getOutputSize();
+				if (trainInLenght != netInLenght || trainOutLenght != netOutLength) {
+					Logger.warn(this.getClass(), "Trainingsdaten {} Input {} Output passten nicht zur Topology {} Input und {} Output",
+							trainInLenght, trainOutLenght, netInLenght, netOutLength);
+					return;
+				}
+
+				Logger.info(this.getClass(), "start working module");
+				net.getWorkingModule().work(net.getNetwork(), testData);
+
+				Logger.info(this.getClass(), testData.toString());
+				GraphController.getInstance().repaint();
+			}
+		});
 	}
 
 	/*
